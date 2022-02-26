542ab060e13d8e9e45a4035757458d2256a160e0
Kasidis Chantharojwong <77087017+ckasidis@users.noreply.github.com>
2022-02-27 04:32:14 +0800

    modify GridCard style

--- components/common/GridCard.tsx
+++ components/common/GridCard.tsx
@@ -23,17 +23,19 @@ const GridCard: NextPage<Props> = ({
 		<div className={`${bgColor} grid gap-4 justify-center py-10 text-center`}>
 			<div className="text-7xl mx-auto text-gray-50">{createElement(icon)}</div>
 			<h2 className="text-3xl font-bold text-gray-50">{title}</h2>
-			<div
-				className="bg-gray-50 w-32 py-2 hover:bg-emerald-400 rounded-full mx-auto"
-				onClick={() => setShowModal(true)}
-			>
-				learn more
-			</div>
-			<Link href={path}>
-				<div className="bg-gray-50 w-32 py-2 hover:bg-emerald-400 rounded-full mx-auto">
-					simulate
+			<div className="flex gap-2 mt-4">
+				<div
+					className="bg-gray-50 w-28 py-2 hover:bg-emerald-400 rounded-lg mx-auto"
+					onClick={() => setShowModal(true)}
+				>
+					learn more
 				</div>
-			</Link>
+				<Link href={path}>
+					<div className="bg-gray-50 w-28 py-2 hover:bg-emerald-400 rounded-lg mx-auto">
+						simulate
+					</div>
+				</Link>
+			</div>
 			{showModal && (
 				<Modal handleClose={() => setShowModal(false)}>{children}</Modal>
 			)}
